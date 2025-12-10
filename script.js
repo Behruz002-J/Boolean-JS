6040c304fad205a594a3fbf645006d1af92e1a9d
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-10 15:20:33 +0500

    added new changes

---
+++ script.js
@@ -0,0 +1,122 @@
+// Integer👇
+//12----
+// let a = 235;
+// n = a % 10;
+
+// m = parseInt(a / 100);
+// d = parseInt(a % 100/10);
+// console.log( " a ning teskarisi = " , n * 100 + d * 10 + m * 1);
+
+//13----
+
+// let k = 321;
+
+// let s = k % 10;
+// let h = parseInt( k / 100 % 10);
+// let b = parseInt(k % 100);
+
+// console.log(b * 10 + h * 1);
+
+//14----
+
+// let n = 345;
+// let s = n % 10;
+// console.log(s);
+// let d = parseInt(n /10);
+// console.log(s *100 + d * 1 );
+
+//Boolean👇
+
+
+//1----
+// let isSon = 5;
+// let result = isSon > 0;
+// console.log(result);
+
+//2----
+// let toqSon = 3;
+// let toq = toqSon > 0;
+// console.log(toq + " 3 toq");
+
+//3----
+// let A = 4;
+// console.log(A < 10 );
+
+//4----
+// let A = 4;
+// let B = 3;
+// let n = A < B;
+// console.log(n);
+ 
+//5----
+
+// let A = 1;
+// let B = -1;
+// console.log(A > B || A < B);
+
+//6----
+// let M = 8;
+// let N = 4;
+// let L = 3;
+// let jackPOT = M <= N <= L
+// console.log(jackPOT);
+
+//7----
+// let a = 4;
+// let b = 3;
+// let c = 5;
+// let result = a < c < b;
+// console.log(result);
+
+//8----
+let A = 9;
+let B = 13;
+let result = A % 2 === 1 && B % 2 === 1;
+console.log(result);
+
+//9----
+let a = 4;
+let b = 1;
+let ever = a % 2 === 0 || b % 1 === 1;
+console.log(ever);
+
+//10----
+let d = 3;
+let e = 6;
+let odd = d % 2 === 0 || e % 1 === 1;
+console.log( "it's  a number = " +  ever);
+
+//THE END
+
+//IF👇
+
+//6----
+let son1 = 13;
+let son2 = 15;
+
+if(son1 > son2) {
+    console.log("son kichik ");
+    
+}else {
+    console.log( " katta son  = " + son2 );
+    
+}
+
+//7----
+let big = 1200;
+let small = 344;
+console.log("small tartib raqami =" + 107);
+//Ustoz tushuntirgandek
+
+//8----
+let K = 32;
+let L = 36;
+if(L > K ) {
+    console.log(L,K);
+}else {
+    console.log(K,L);
+}
+
+//FOR👇
+
+//6----
